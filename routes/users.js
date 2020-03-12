914dd3631f88e8cd216b7a976e7771429d0d67eb
Martin Horváth <martin.horvath@attrecto.com>
2020-03-12 11:47:12 +0100

    4/1 - Create placeholder user routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET users listing. */
+/* GET all users. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('GET all users');
+});
+
+/* GET one user. */
+router.get('/:id', function(req, res, next) {
+  res.send('GET one user with id: ' + req.params.id);
+});
+
+/* CREATE user. */
+router.post('/', function(req, res, next) {
+  res.send('CREATE user');
+});
+
+/* MODIFY user. */
+router.patch('/:id', function(req, res, next) {
+  res.send('MODIFY user with id: ' + req.params.id);
+});
+
+/* DELETE user. */
+router.delete('/:id', function(req, res, next) {
+  res.send('DELETE user with id: ' + req.params.id);
 });
 
 module.exports = router;
